d7c7b320ffe2d25600724d96d237e5e65517f9a8
Kai191911 <suvijack.b@gmail.com>
2025-11-08 23:52:11 +0700

    Create test-redis.js

---
+++ api/test-redis.js
@@ -0,0 +1,12 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  await redis.set("hello", "world");
+  const value = await redis.get("hello");
+  res.json({ value });
+}
